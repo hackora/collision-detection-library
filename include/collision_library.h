ad3b263a15af6eb2d7787b3878e8bdb0e31035a3
ghada <gbo013@post.uit.no>
2017-01-20 13:00:35 +0100

    tests of collision detection and response  pass

--- include/collision_library.h
+++ include/collision_library.h
@@ -6,12 +6,70 @@
 
 
 
-namespace collision
-{
+   namespace collision
+   {
+
+   class MyController : public Controller {
+       GM_SCENEOBJECT (MyController)
+   public:
+
+       explicit MyController () = default;
+
+       void add (DynamicPSphere* const sphere) { _dynamic_spheres.push_back(sphere); }
+       void add (StaticPSphere* const sphere) { _static_spheres.push_back(sphere); }
+       void add (StaticPPlane* const plane) { _static_planes.push_back(plane); }
+       void add (StaticPCylinder* const cylinder) { _static_cylinders.push_back(cylinder); }
+       void add (StaticPBezierSurf* const surf) { _static_bezier_surf.push_back(surf); }
+
+   protected:
+       void localSimulate (double dt) override;
+
+       std::vector<DynamicPSphere*>    _dynamic_spheres;
+       std::vector<StaticPSphere*>     _static_spheres;
+       std::vector<StaticPPlane*>      _static_planes;
+       std::vector<StaticPCylinder*>   _static_cylinders;
+       std::vector<StaticPBezierSurf*> _static_bezier_surf;
+
+       std::vector<collision::CollisionObject> _collisions;
+
+   };
+
+   template <>
+   class DynamicPhysObject<GMlib::PSphere<float>> : public DynamicPhysObject_Base<GMlib::PSphere<float>> {
+   public:
+       using DynamicPhysObject_Base<GMlib::PSphere<float>>::DynamicPhysObject_Base;
+
+       void    simulateToTInDt( seconds_type t ) override {
+           this->curr_t_in_dt =t;
+       }
 
 
-} // END namespace collision
+       GMlib::Vector<float,3> computeTrajectory( seconds_type dt) const override { //m
+
+           auto const tay = 0.5 * this->externalForces() * dt.count()* dt.count(); //taylor
+           return this->velocity * dt.count() + tay  ;
+       }
 
 
+       GMlib::Vector<float, 3> externalForces () const override ; // [m / s^2]
+   };
 
+   template <class PSurf_T, typename... Arguments>
+   std::unique_ptr<DynamicPhysObject<PSurf_T>> unittestDynamicPhysObjectFactory(Arguments... parameters) {
+
+       return std::make_unique<DynamicPhysObject<PSurf_T>> (parameters...);
+
+   }
+
+   template <class PSurf_T, typename... Arguments>
+   std::unique_ptr<StaticPhysObject<PSurf_T>> unittestStaticPhysObjectFactory(Arguments... parameters) {
+
+       return std::make_unique<StaticPhysObject<PSurf_T>> (parameters...);
+   }
+
+   template <class Container_T > void sortAndMakeUnique( Container_T& container) { }
+
+   //std::unique_ptr<Controller> unittestCollisionControllerFactory(){ return std::make_unique<MyController> (); }
+   }
+   // END namespace collision
 #endif //COLLISION_LIBRARY_H
--- sources/collision_library.cpp
+++ sources/collision_library.cpp
@@ -1,6 +1,6 @@
 #include "collision_library.h"
-
-
+#include <iostream>
+#include <cmath>
 
 namespace collision
 {
@@ -11,13 +11,40 @@ namespace collision
                      const DynamicPhysObject<GMlib::PSphere<float>>& S1,
                      seconds_type                                    dt)
     {
-    }
 
-    CollisionState
-    detectCollision (const DynamicPhysObject<GMlib::PSphere<float>>& S0,
-                     const StaticPhysObject<GMlib::PSphere<float>>&  S1,
-                     seconds_type                                    dt)
-    {
+        auto max_dt = dt;
+        auto min_dt = max(S0.curr_t_in_dt, S1.curr_t_in_dt );
+        auto new_dt = max_dt -min_dt;
+        auto p0 = S0.getMatrixToScene() *S0.getPos();
+        auto r0 = S0.getRadius();
+        auto p1 = S1.getMatrixToScene() *S1.getPos();
+        auto r1 = S1.getRadius();
+        auto r = r0 + r1;
+        auto Q = p1 - p0;
+        auto R = S1.computeTrajectory(new_dt) - S0.computeTrajectory(new_dt);
+        auto a = R * R;
+        auto b = Q * R;
+        auto c = (Q * Q) - r*r;
+        auto epsilon = 1e-6;
+
+        if ((std::abs(c))< epsilon)
+        {
+            return(CollisionState(seconds_type (0.0),CollisionStateFlag::SingularityParallelAndTouching));
+        }
+        else if ((std::abs(a))< epsilon)
+        {
+            return(CollisionState(seconds_type (0.0),CollisionStateFlag::SingularityParallel));
+        }
+        else if ((b*b - a*c)< 0)
+        {
+            return(CollisionState(seconds_type (0.0),CollisionStateFlag::SingularityNoCollision));
+        }
+        else
+        {
+            auto x = (-b - sqrt(b*b - a*c))/a;
+            return (CollisionState (x*new_dt +min_dt,CollisionStateFlag::Collision));
+        }
+
     }
 
     CollisionState
@@ -25,13 +52,98 @@ namespace collision
                      const StaticPhysObject<GMlib::PPlane<float>>&   P,
                      seconds_type                                    dt)
     {
+
+        auto max_dt = dt;
+        auto min_dt = S.curr_t_in_dt;
+        auto new_dt = max_dt -min_dt;
+        auto p = S.getMatrixToScene() * S.getPos();
+        auto r = S.getRadius();
+
+        auto unconst_P = const_cast <StaticPhysObject<GMlib::PPlane<float>>&>(P);
+        const auto M = unconst_P.evaluateParent(0.5f,0.5f,1,1);
+        auto q = M(0)(0);
+        auto u = M(1)(0);
+        auto v = M(0)(1);
+        auto n = GMlib::Vector<float,3>(u ^ v).getNormalized();
+        auto d = (q + r * n) - p;
+
+        auto ds = S.computeTrajectory(new_dt);
+        auto epsilon = 1e-6;
+
+        if ((std::abs(d * n))< epsilon)
+                {
+                    return(CollisionState(seconds_type (0.0),CollisionStateFlag::SingularityParallelAndTouching));
+                }
+
+        else if ((std::abs(ds * n))< epsilon)
+        {
+            return(CollisionState(seconds_type (0.0),CollisionStateFlag::SingularityParallel));
+        }
+
+        else
+        {
+            auto x = (d * n) / (ds * n);
+            return (CollisionState (x*new_dt +min_dt,CollisionStateFlag::Collision));
+        }
+
     }
 
+
     CollisionState
-    detectCollision (const DynamicPhysObject<GMlib::PSphere<float>>&  S,
-                     const StaticPhysObject<GMlib::PCylinder<float>>& C,
+    detectCollision (const DynamicPhysObject<GMlib::PSphere<float>>& S,
+                     const StaticPhysObject<GMlib::PBezierSurf<float>>& B,
                      seconds_type                                    dt)
     {
+
+        auto max_dt = dt;
+        auto min_dt = S.curr_t_in_dt;
+        auto new_dt = max_dt -min_dt;
+        float u,v,t,delta_u,delta_v,delta_t;
+        u = 0.5;
+        v = 0.5;
+        t = 0.0;
+        delta_u = 0.5;
+        delta_v = 0.5;
+        delta_t = 0.0;
+        auto p0 = S.getMatrixToScene() *S.getPos();
+        auto r = S.getRadius();
+        auto unconst_B = const_cast <StaticPhysObject<GMlib::PBezierSurf<float>>&>(B);
+        GMlib::Vector<float, 3> ds = S.computeTrajectory(new_dt);
+        GMlib::SqMatrix<float,3> A;
+
+        //iteration
+
+        for (int i=0; i<=5;i++){
+        auto p = p0 + ds*t;
+        const auto M = unconst_B.evaluateParent(u,v,1,1);
+        auto q = M(0)(0);
+        const auto Su = M(1)(0);
+        const  auto Sv = M(0)(1);
+        auto Sn = GMlib::Vector<float,3> (Su ^ Sv).getNormalized();
+        A.setCol(Su,0);
+        A.setCol(Sv,1);
+        A.setCol(-ds,2);
+        GMlib::SqMatrix<float,3> A_inv = A;
+        A_inv.invert();
+        GMlib::APoint<float, 3> b = GMlib::Vector<float, 3> {p-q-Sn*r};
+        GMlib::APoint<float, 3> X = A_inv*b;
+        delta_u = X(0);
+        delta_v = X(1);
+        delta_t = X(2);
+        u += delta_u;
+        v += delta_v;
+        t += delta_t;
+
+        auto epsilon = 1e-5;
+
+        if ( (delta_t < epsilon) && (std::abs(delta_u) < epsilon) && (std::abs(delta_v)< epsilon))
+        {
+            return (CollisionState (seconds_type(delta_t),CollisionStateFlag::Collision));
+        }
+
+        }
+        return(CollisionState(seconds_type (0.0),CollisionStateFlag::SingularityNoCollision));
+
     }
 
 
@@ -41,27 +153,74 @@ namespace collision
                            DynamicPhysObject<GMlib::PSphere<float>>& S1,
                            seconds_type                              dt)
     {
-    }
+        /*auto p0 = S0.getPos();
+        auto r0 = S0.getRadius();
+        auto p1 = S1.getPos();
+        auto r1 = S1.getRadius();
 
-    void
-    computeImpactResponse (DynamicPhysObject<GMlib::PSphere<float>>& S0,
-                           StaticPhysObject<GMlib::PSphere<float>>&  S1,
-                           seconds_type                              dt)
-    {
+        auto d = GMlib::Vector<float,3>(p1 -p0);
+        auto n = GMlib::Vector<float,3>(d ^ d.getLinIndVec()).getNormalized();
+        auto v0d = (S0.velocity * d);
+        auto v1d = (S1.velocity * d);
+        auto v0n = v0d * n;
+        auto v1n = v1d * n;
+        auto new_v0d = v0d * (S0.mass - S1.mass)/(S0.mass + S1.mass)
+                     + v1d * 2 * S1.mass / (S0.mass + S1.mass);
+        auto new_v1d = v1d * (S1.mass - S0.mass)/(S0.mass + S1.mass)
+                     + v0d * 2 * S0.mass / (S0.mass + S1.mass);
+
+        S0.velocity = GMlib::Vector<float,3>(v0n * n) + (new_v0d * d.getNormalized());
+        S1.velocity = GMlib::Vector<float,3>(v1n * n) + (new_v1d * d.getNormalized());*/
+
+        const auto S0_old_vel = S0.velocity;
+        const auto S1_old_vel = S1.velocity;
+        const auto S0_pos = S0.getPos();
+        const auto S1_pos = S1.getPos();
+        const auto S0_mass = S0.mass;
+        const auto S1_mass = S1.mass;
+        const auto distance_vector_d = GMlib::Vector<float,3>(S1_pos - S0_pos);
+        const auto normal_d = distance_vector_d.getNormalized();
+        const auto n = (GMlib::Vector<float,3>(distance_vector_d).getLinIndVec()).getNormalized();
+        const auto v0_d = (S0_old_vel * normal_d);
+        const auto v1_d = (S1_old_vel * normal_d);
+        const auto v0_n = (S0_old_vel * n);
+        const auto v1_n = (S1_old_vel * n);
+        const auto new_v0_d = (((S0_mass - S1_mass) / (S0_mass + S1_mass) ) * v0_d ) + (((2 * S1_mass) / (S0_mass + S1_mass) ) * v1_d );
+        const auto new_v1_d = (((S1_mass - S0_mass) / (S0_mass + S1_mass) ) * v1_d ) + (((2 * S0_mass) / (S0_mass + S1_mass) ) * v0_d );
+        const auto S0_new_vel = (v0_n * n) + (new_v0_d * normal_d);
+        auto S1_new_vel = v1_n * n + new_v1_d * normal_d;
+        S0.velocity = S0_new_vel;
+        S1.velocity = S1_new_vel;
+
     }
 
     void
     computeImpactResponse (DynamicPhysObject<GMlib::PSphere<float>>& S,
-                           StaticPhysObject<GMlib::PPlane<float>>&   P,
+                           const StaticPhysObject<GMlib::PPlane<float>>&   P,
                            seconds_type                              dt)
     {
+
+        auto unconst_P = const_cast <StaticPhysObject<GMlib::PPlane<float>>&>(P);
+        const auto M = unconst_P.evaluateParent(0.5f,0.5f,1,1);
+        auto u = M(1)(0);
+        auto v = M(0)(1);
+        auto n = GMlib::Vector<float,3>(u ^ v).getNormalized();
+        auto vel = S.velocity * n;
+
+        auto new_velocity = S.velocity - ((2* vel)*n);
+        S.velocity = new_velocity ;
     }
 
-    void
-    computeImpactResponse (DynamicPhysObject<GMlib::PSphere<float>>&  S,
-                           StaticPhysObject<GMlib::PCylinder<float>>& C,
-                           seconds_type                              dt)
-    {
+
+    std::unique_ptr<Controller> unittestCollisionControllerFactory(){ return std::make_unique<MyController> (); }
+    //void DynamicPhysObject<GMlib::PSphere<float> >::simulateToTInDt(seconds_type t){ }
+
+    GMlib::Vector<float,3> DynamicPhysObject<GMlib::PSphere<float> >::externalForces() const {
+        assert (environment != nullptr);
+        return this->environment->externalForces();
+
     }
+
+    void MyController::localSimulate(double dt) { }
 } // END namespace collision
 
