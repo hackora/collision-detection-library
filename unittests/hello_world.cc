17b5b1db0afbe2531e048beec25f93a28eb97c9a
ghada <gbo013@post.uit.no>
2017-01-25 10:14:48 +0100

    all make unique tests pass

--- unittests/hello_world.cc
+++ unittests/hello_world.cc
@@ -152,3 +152,186 @@ TEST(SortAndMakeUnique, MakeUnique) {
   EXPECT_EQ(c1.obj1, sphere3.get()); EXPECT_EQ( c1.t_in_dt, seconds_type{0.2ms} );
   EXPECT_EQ(c2.obj1, sphere2.get()); EXPECT_EQ( c2.t_in_dt, seconds_type{0.4ms} );
 }
+
+
+
+//adapted from interface tests
+
+
+
+TEST(SortAndMakeUnique, CompareFirstToFirst1) {
+
+  auto sphere1 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere2 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+
+  auto cylinder1 = unittestStaticPhysObjectFactory<PCylinder<float>>();
+
+  std::multimap<seconds_type,CollisionObject> collisions;
+
+  collisions.emplace(seconds_type{0.1ms},CollisionObject(sphere1.get(), cylinder1.get(), 0.1ms ));
+  collisions.emplace(seconds_type{0.2ms}, CollisionObject(sphere2.get(), cylinder1.get(), 0.2ms ));
+  collisions.emplace(seconds_type{0.3ms}, CollisionObject(sphere1.get(), cylinder1.get(), 0.3ms ));
+
+  sortAndMakeUnique(collisions);
+
+  int i=0;
+  CollisionObject c0( sphere1.get(), cylinder1.get(), 0.1ms );
+  CollisionObject c1( sphere1.get(), cylinder1.get(), 0.1ms );
+  for (auto& it: collisions){
+      if (i==0){
+          c0 = it.second;
+          i++;
+      }
+      else {
+          c1 = it.second;
+      }
+  }
+
+  EXPECT_EQ(collisions.size(), 2);
+  EXPECT_EQ(c0.obj1, sphere1.get());
+  EXPECT_EQ(c1.obj1, sphere2.get());
+}
+
+TEST(SortAndMakeUnique, CompareFirstToSecond1) {
+
+  auto sphere1 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere2 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere3 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+
+  auto cylinder1 = unittestStaticPhysObjectFactory<PCylinder<float>>();
+
+  std::multimap<seconds_type,CollisionObject> collisions;
+
+  collisions.emplace(seconds_type{0.1ms},CollisionObject(sphere1.get(), cylinder1.get(), 0.1ms ));
+  collisions.emplace(seconds_type{0.2ms}, CollisionObject(sphere2.get(), cylinder1.get(), 0.2ms ));
+  collisions.emplace(seconds_type{0.3ms}, CollisionObject(sphere2.get(), sphere1.get(), 0.3ms ));
+
+  sortAndMakeUnique(collisions);
+  int i=0;
+  CollisionObject c0( sphere1.get(), cylinder1.get(), 0.1ms );
+  CollisionObject c1( sphere1.get(), cylinder1.get(), 0.1ms );
+  for (auto& it: collisions){
+      if (i==0){
+          c0 = it.second;
+          i++;
+      }
+      else {
+          c1 = it.second;
+      }
+  }
+
+
+  EXPECT_EQ(collisions.size(), 2);
+  EXPECT_EQ(c0.obj1, sphere1.get());
+  EXPECT_EQ(c1.obj1, sphere2.get());
+
+}
+
+TEST(SortAndMakeUnique, CompareSecondToFirst1) {
+
+  auto sphere1 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere2 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere3 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+
+  auto cylinder1 = unittestStaticPhysObjectFactory<PCylinder<float>>();
+
+  std::multimap<seconds_type,CollisionObject> collisions;
+
+  collisions.emplace(seconds_type{0.1ms},CollisionObject(sphere1.get(), sphere2.get(), 0.1ms ));
+  collisions.emplace(seconds_type{0.2ms}, CollisionObject(sphere3.get(), cylinder1.get(), 0.2ms ));
+  collisions.emplace(seconds_type{0.3ms}, CollisionObject(sphere2.get(), cylinder1.get(), 0.3ms ));
+
+  sortAndMakeUnique(collisions);
+
+  int i=0;
+  CollisionObject c0( sphere1.get(), cylinder1.get(), 0.1ms );
+  CollisionObject c1( sphere1.get(), cylinder1.get(), 0.1ms );
+  for (auto& it: collisions){
+      if (i==0){
+          c0 = it.second;
+          i++;
+      }
+      else {
+          c1 = it.second;
+      }
+  }
+
+  EXPECT_EQ(collisions.size(), 2);
+  EXPECT_EQ(c0.obj1, sphere1.get());
+  EXPECT_EQ(c1.obj1, sphere3.get());
+
+}
+
+TEST(SortAndMakeUnique, CompareSecondToSecond) {
+
+  auto sphere1 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere2 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere3 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere4 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+
+  auto cylinder1 = unittestStaticPhysObjectFactory<PCylinder<float>>();
+
+  std::multimap<seconds_type,CollisionObject> collisions;
+
+  collisions.emplace(seconds_type{0.1ms},CollisionObject(sphere1.get(), sphere2.get(), 0.1ms ));
+  collisions.emplace(seconds_type{0.2ms}, CollisionObject(sphere3.get(), cylinder1.get(), 0.2ms ));
+  collisions.emplace(seconds_type{0.3ms}, CollisionObject(sphere4.get(), sphere2.get(), 0.3ms ));
+
+  sortAndMakeUnique(collisions);
+
+  int i=0;
+  CollisionObject c0( sphere1.get(), cylinder1.get(), 0.1ms );
+  CollisionObject c1( sphere1.get(), cylinder1.get(), 0.1ms );
+  for (auto& it: collisions){
+      if (i==0){
+          c0 = it.second;
+          i++;
+      }
+      else {
+          c1 = it.second;
+      }
+  }
+
+  EXPECT_EQ(collisions.size(), 2);
+  EXPECT_EQ(c0.obj1, sphere1.get());
+  EXPECT_EQ(c1.obj1, sphere3.get());
+
+}
+
+TEST(SortAndMakeUnique, ComplexExample) {
+
+
+  auto sphere1 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere2 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+  auto sphere3 = unittestDynamicPhysObjectFactory<PSphere<float>>();
+
+  auto cylinder = unittestStaticPhysObjectFactory<PCylinder<float>>();
+
+  std::multimap<seconds_type,CollisionObject> collisions;
+
+  collisions.emplace(seconds_type{0.35ms},CollisionObject(sphere1.get(), cylinder.get(), 0.35ms ));
+  collisions.emplace(seconds_type{0.3ms}, CollisionObject(sphere2.get(), cylinder.get(), 0.3ms ));
+  collisions.emplace(seconds_type{0.1ms}, CollisionObject(sphere1.get(), sphere3.get(), 0.1ms ));
+  collisions.emplace(seconds_type{0.11ms},CollisionObject(sphere3.get(), sphere1.get(), 0.11ms ));
+  collisions.emplace(seconds_type{0.41ms}, CollisionObject(sphere2.get(), sphere3.get(), 0.41ms ));
+  collisions.emplace(seconds_type{0.4ms}, CollisionObject(sphere1.get(), sphere2.get(), 0.4ms ));
+
+  sortAndMakeUnique(collisions);
+
+  int i=0;
+  CollisionObject c0( sphere1.get(), cylinder.get(), 0.1ms );
+  CollisionObject c1( sphere1.get(), cylinder.get(), 0.1ms );
+
+  for (auto& it: collisions){
+      if (i==0){
+          c0 = it.second;
+          i++;
+      }
+      else {
+          c1 = it.second;
+      }
+  }
+
+  EXPECT_EQ(c0.obj1, sphere1.get()); EXPECT_EQ(c0.obj2, sphere3.get()); EXPECT_EQ( c0.t_in_dt, seconds_type{0.1ms} );
+  EXPECT_EQ(c1.obj1, sphere2.get()); EXPECT_EQ(c1.obj2, cylinder.get()); EXPECT_EQ( c1.t_in_dt, seconds_type{0.3ms} );
+}
